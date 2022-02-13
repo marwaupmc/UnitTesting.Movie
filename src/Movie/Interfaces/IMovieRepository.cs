2327d37229292c719bcf74f1d15400fbe3c85f58
Marwa CHEBIL <cmarwa@kpmg.fr>
2022-02-13 15:57:24 +0100

    * Add Business project + Add model classes & interface.

---
+++ src/Movie/Interfaces/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IMovieRepository.cs" company="MarwaCHEBIL">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Movie
+{
+    public interface IMovieRepository
+    {
+        Task<Movie> GetMovieByIdAsync(Guid id);
+
+        Task SaveMovieAsync(Movie movie);
+
+        Task DeleteMovieAsync(Movie movie);
+    }
+}
---
+++ src/Movie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Movie.cs" company="MarwaCHEBIL">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Movie
+{
+    public class Movie
+    {
+        public Movie(Guid id, string title, DateTime? releaseDate)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.ReleaseDate = releaseDate;
+        }
+
+        public Guid Id { get; }
+
+        public string Title { get; }
+
+        public DateTime? ReleaseDate { get; }
+    }
+}
